16e92e26224f2e59b22c68f7886064e2a30a842c
Tenal <tenalbourchier@gmail.com>
2020-12-05 12:35:34 -0500

    added an article section

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Barrie Test</h1>
-        <h2>More testing</h2>
+      <div>
+        <div className="App">
+          <h1>Barrie Test</h1>
+          <h2>More testing</h2>
+        </div>
+        <article className="first-article">
+          <p>hello this is an article</p>
+        </article>
       </div>
     );
   }
