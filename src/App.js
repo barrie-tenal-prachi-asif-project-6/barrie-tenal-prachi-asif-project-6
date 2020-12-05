a7cb8f1c28c2fd1e5304790383278415d2505cac
Prachi Gujar <prachigujar@Prachis-MacBook-Air.local>
2020-12-05 12:40:10 -0500

    added header mainHeading

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="App">
-          <h1>Barrie Test</h1>
-          <h2>More testing</h2>
-        </div>
+        <header className="mainHeading">
+          <div className="App">
+            <h1>Barrie Test</h1>
+            <h2>More testing</h2>
+          </div>
+        </header>
+        
         <article className="first-article">
           <p>hello this is an article</p>
         </article>
