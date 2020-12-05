5d1ca10e9a68bd97f2335dd99eae9e1a5e6e7032
Tenal <tenalbourchier@gmail.com>
2020-12-05 18:40:36 -0500

    zoltar SPEAKS!! installed animate.css, linked it to idnex.html, & used keyframes to move zoltar's mouth :fire:

--- src/components/Zoltar.js
+++ src/components/Zoltar.js
@@ -1,8 +1,11 @@
 const Zoltar = () => {
     return (
-        <div className="zoltar-image">
-            <img src="https://i.ibb.co/FxMnytF/zoltar-head.jpg" alt="zoltar-head" border="0" />
-            <img className="zoltar-mouth" src="https://i.ibb.co/4myFDDZ/zoltar-mouth.png" alt="zoltar-mouth" border="0"/>
+        <div className="zoltar-image-container">
+            <div className="zoltar-image">
+                <img src="https://i.ibb.co/FxMnytF/zoltar-head.jpg" alt="zoltar-head" border="0" />
+                {/* PREVIOUSLY using animation gallery which will be useful for later with appearing text box // animate__animated animate__shakeY animate__slower animate__infinite */}
+                <img className="zoltar-mouth " src="https://i.ibb.co/4myFDDZ/zoltar-mouth.png" alt="zoltar-mouth" border="0"/>
+            </div>
         </div>
     );
 }
