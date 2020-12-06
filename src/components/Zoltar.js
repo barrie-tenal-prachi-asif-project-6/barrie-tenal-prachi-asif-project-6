19a0c8a30cc30c8868f1703e4912e89777f22bb5
Tenal <tenalbourchier@gmail.com>
2020-12-05 19:26:46 -0500

    created a list of possible API keywords

--- src/components/Zoltar.js
+++ src/components/Zoltar.js
@@ -3,7 +3,7 @@ const Zoltar = () => {
         <div className="zoltar-image-container">
             <div className="zoltar-image">
                 <img src="https://i.ibb.co/FxMnytF/zoltar-head.jpg" alt="zoltar-head" border="0" />
-                {/* PREVIOUSLY using animation gallery which will be useful for later with appearing text box // animate__animated animate__shakeY animate__slower animate__infinite */}
+                {/* initially used animation gallery which I installed with 'npm i animate.css --save' (linked in index.html & will be useful for later with appearing text box) */}
                 <img className="zoltar-mouth " src="https://i.ibb.co/4myFDDZ/zoltar-mouth.png" alt="zoltar-mouth" border="0"/>
             </div>
         </div>
@@ -11,3 +11,40 @@ const Zoltar = () => {
 }
 
 export default Zoltar;
+
+
+
+// Advice slip API keywords
+
+// MULTIPLE results:
+    // life (11)
+    // love (5)
+    // friends (3)
+    // happiness (3)
+    // learn (4) - 'learning'
+    // old (6) - 'aging'
+    // art (4)
+    // food (3)
+    // feeling (2) - 'emotions'
+
+
+// LIMITED results:
+    // money (1)
+    // exercise (1)
+    // fun (2) - one might be deemed 'inappropriate', not sure the client is aware of it lol
+    // dog (1), horse (1), probably other random animals... but not 'animal' lol
+
+
+// NO results (what even is this api?!):
+    // health
+    // relationships
+    // travel
+    // family
+    // sibling/sister/brother
+    // religion
+    // career
+    // laugh/funny/joke/humor(ous)
+    // school/study(ing)
+    // animal
+    // energy/activity
+    // emotion(s/al)
