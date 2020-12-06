cf23afb72444d3bfb509612c12d0e14062fd8742
Prachi Gujar <prachigujar@Prachis-MacBook-Air.local>
2020-12-06 18:43:26 -0500

    updating the local branch

--- src/App.js
+++ src/App.js
@@ -6,21 +6,9 @@ import Axios from 'axios';
 
 class App extends Component {
 
-  
-
-
   render() {
     return (
       <div>
-        <div className="App">
-          <header className="wrapper">
-            <h1>Barrie Test</h1>
-            <h2>More testing</h2>
-          </header>
-        </div>
-        <article className="first-article">
-          <p>hello this is an article</p>
-        </article>
         <Zoltar />
         <Maze />
       </div>
@@ -30,22 +18,3 @@ class App extends Component {
 export default App;
 
 
-// if the url is 'https://api.adviceslip.com/advice' the output needs to be either assigned as an this.state.adviceSlip and this.state.adviceKey or can be pushed to an empty array( but knowing we need only one advice, we do not need to map through the array as this url returns only one object)
-
-// axios call for this url:
-// getRandomAdvice = ()=>{
-//   Axios({
-//     // url: 'https://api.adviceslip.com/advice',
-//     url: ' 	https://api.adviceslip.com/advice/search/other',
-//     GET: 'Method',
-//     responseType: 'JSON'
-//   })
-//     .then((res) => {
-//       console.log(res.data.slip);
-//       const newAdvice = []
-//       newAdvice.push(res.data.slip)
-//       this.setState({
-//         adviceSlip: newAdvice
-//       })
-//     })
-// }
