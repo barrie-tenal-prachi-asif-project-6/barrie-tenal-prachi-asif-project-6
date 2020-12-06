8d89d5c5a8647d6664e90efe74ff94cb1e6eee3a
Tenal <tenalbourchier@gmail.com>
2020-12-06 12:18:42 -0500

    updated file structure

--- pseudo.js
+++ pseudo.js
@@ -52,3 +52,42 @@
             // Firebase (OPTIONAL)
             // react-router-dom 
     
+
+
+
+// Advice slip API keywords
+
+// MULTIPLE results:
+    // life (11)
+    // people (11)
+    // work (7)
+    // love (5)
+    // friends (3)
+    // happiness (3)
+    // learn (4) - 'learning'
+    // old (6) - 'aging'
+    // art (4)
+    // food (3)
+    // feeling (2) - 'emotions'
+
+
+// LIMITED results:
+    // money (1)
+    // exercise (1)
+    // fun (2) - one might be deemed 'inappropriate', not sure the client is aware of it lol
+    // dog (1), horse (1), probably other random animals... but not 'animal' lol
+
+
+// NO results (what even is this api?!):
+    // health
+    // relationships
+    // travel
+    // family
+    // sibling/sister/brother
+    // religion
+    // career
+    // laugh/funny/joke/humor(ous)
+    // school/study(ing)
+    // animal
+    // energy/activity
+    // emotion(s/al)
