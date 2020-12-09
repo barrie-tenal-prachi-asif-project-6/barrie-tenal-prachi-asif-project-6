4dee8491385189b80cc81dd880e8551d496a4f0e
Prachi Gujar <prachigujar@Prachis-MacBook-Air.local>
2020-12-08 21:54:07 -0500

    added corner pieces to reflect changes from instructions

--- src/components/Results.js
+++ src/components/Results.js
@@ -15,15 +15,19 @@ class Results extends Component {
       <div className="Results">
         <ZoltarRedEyes />
         <div className="wishContainer wrapper">
-          <h2>{this.props.userName}, your wish has been granted! </h2>
-          <p>
-            {
-              this.randomizer(this.props.adviceSlip)
-            }
-          </p>
-          <Link to="/">
-            <button className="resetWish">Make another wish!</button>
-          </Link>
+          <img className="cornerImage topLeftCornerImage" src="https://i.ibb.co/C5yk8W2/corner.png" alt="a decorative corner drawing of an abstract bird" />
+          <img className="cornerImage topRightCornerImage" src="https://i.ibb.co/C5yk8W2/corner.png" alt="a decorative corner drawing of an abstract bird" />
+          <img className="cornerImage bottomRightCornerImage" src="https://i.ibb.co/C5yk8W2/corner.png" alt="a decorative corner drawing of an abstract bird" />
+          <img className="cornerImage bottomLeftCornerImage" src="https://i.ibb.co/C5yk8W2/corner.png" alt="a decorative corner drawing of an abstract bird" />
+            <h2>{this.props.userName}, your wish has been granted! </h2>
+            <p>
+              {
+                this.randomizer(this.props.adviceSlip)
+              }
+            </p>
+            <Link to="/">
+              <button className="resetWish">Make another wish</button>
+            </Link>
         </div>
       </div>
     );
