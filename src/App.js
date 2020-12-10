9dfda2c6b03dafbef0769b34ca04efe623752f82
Asif <asif14.k@hotmail.com>
2020-12-10 14:44:11 -0500

    Fixed refresh so it displays the homepage.

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router basename={process.env.PUBLIC_URL}>
+      <Router basename={process.env.PUBLIC_URL + '/'}>
         <>
           <Header />
           <main>
