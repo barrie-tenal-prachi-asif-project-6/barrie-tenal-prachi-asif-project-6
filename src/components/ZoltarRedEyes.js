8fe69f78f61feacd0fe6989ed0cddc9b7b854029
Asif <asif14.k@hotmail.com>
2020-12-10 15:15:58 -0500

    Added new image for zoltar's red eyes.

--- src/components/ZoltarRedEyes.js
+++ src/components/ZoltarRedEyes.js
@@ -2,7 +2,7 @@ const ZoltarRedEyes = () => {
     return (
         <div className="zoltarImageContainer">
             <div className="zoltarImage">
-                <img className="zoltarRedEyes" src="https://i.ibb.co/f4pGc8D/zoltar-red.jpg" alt="zoltar with red eyes" border="0" />
+                <img className="zoltarRedEyes" src="https://i.ibb.co/Qp6wSJD/zoltar-red.jpg" alt="zoltar with red eyes" border="0" />
                 <img src="https://i.ibb.co/1JhdhBm/zoltar.jpg" alt="zoltar body" border="0" />
                 <img className="zoltarRedEyesMouth" src="https://i.ibb.co/C6TQK03/zoltar-Jaw.png" alt="zoltar mouth" border="0" />
             </div>
